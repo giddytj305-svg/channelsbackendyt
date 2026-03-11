58b04ae82882f61d6479ef27ea1e052661e09403
giddytj305-svg <giddytj305@gmail.com>
2026-03-11 17:32:51 +0300

    Create channels.js

---
+++ data/channels.js
@@ -0,0 +1,96 @@
+// Preloaded TV channels with their YouTube channel IDs
+module.exports = {
+  channels: [
+    // Kenya
+    {
+      id: "citizen-tv-kenya",
+      name: "Citizen TV Kenya",
+      channelId: "UCy9CxDjiASdT2EE0tsgqHpA",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_m9taqAqj_8qM47gJXp5D5R19kC_6DnH4Zq5R-l7w=s176-c-k-c0x00ffffff-no-rj",
+      country: "Kenya",
+      category: "News"
+    },
+    {
+      id: "kbc-kenya",
+      name: "KBC Kenya",
+      channelId: "UCW_hQq3KDskFyPqR_9Y6r9Q",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_lM66Y3vUSL68E4vq7NQkHjqCqXHLZ5GZqL6Z8t=s176-c-k-c0x00ffffff-no-rj",
+      country: "Kenya",
+      category: "News"
+    },
+    {
+      id: "ntv-kenya",
+      name: "NTV Kenya",
+      channelId: "UCJjNfB_JLq5-t-9b0PwIu7A",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_l8R9VqB5cNQkHjqCqXHLZ5GZqL6Z8t=s176-c-k-c0x00ffffff-no-rj",
+      country: "Kenya",
+      category: "News"
+    },
+    
+    // International News
+    {
+      id: "al-jazeera",
+      name: "Al Jazeera English",
+      channelId: "UC5PrJjC2QqRkHjqCqXHLZ5G",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_nJjC2QqRkHjqCqXHLZ5G=s176-c-k-c0x00ffffff-no-rj",
+      country: "Qatar",
+      category: "News"
+    },
+    {
+      id: "bbc-news",
+      name: "BBC News",
+      channelId: "UC16niRr50-MSBwiO3YDb3RA",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_mXHLZ5GZqL6Z8tJjC2QqRkHjqCqXHL=s176-c-k-c0x00ffffff-no-rj",
+      country: "UK",
+      category: "News"
+    },
+    {
+      id: "sky-news",
+      name: "Sky News",
+      channelId: "UCoMdktPbSTQtlJgZzGJx5Aw",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_l8R9VqB5cNQkHjqCqXHLZ5GZqL6Z8t=s176-c-k-c0x00ffffff-no-rj",
+      country: "UK",
+      category: "News"
+    },
+    {
+      id: "france-24",
+      name: "France 24",
+      channelId: "UCQfwfsi5VrQ8yKZ-UgJx5Aw",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_mXHLZ5GZqL6Z8tJjC2QqRkHjqCqXHL=s176-c-k-c0x00ffffff-no-rj",
+      country: "France",
+      category: "News"
+    },
+    {
+      id: "dw-news",
+      name: "DW News",
+      channelId: "UCknLrEdhRCp1aegoMqRaCcA",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_l8R9VqB5cNQkHjqCqXHLZ5GZqL6Z8t=s176-c-k-c0x00ffffff-no-rj",
+      country: "Germany",
+      category: "News"
+    },
+    {
+      id: "cgtn",
+      name: "CGTN",
+      channelId: "UCY6KjD4BLqpGhLZ5GZqL6Z8t",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_mXHLZ5GZqL6Z8tJjC2QqRkHjqCqXHL=s176-c-k-c0x00ffffff-no-rj",
+      country: "China",
+      category: "News"
+    },
+    {
+      id: "nbc-news",
+      name: "NBC News",
+      channelId: "UCeY0bbntWzzVI6ggp8Td4oA",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_l8R9VqB5cNQkHjqCqXHLZ5GZqL6Z8t=s176-c-k-c0x00ffffff-no-rj",
+      country: "USA",
+      category: "News"
+    },
+    {
+      id: "bloomberg",
+      name: "Bloomberg Live",
+      channelId: "UCUMZ7gohGI9HcU9Vqe6-a4w",
+      logo: "https://yt3.googleusercontent.com/ytc/AIdro_mXHLZ5GZqL6Z8tJjC2QqRkHjqCqXHL=s176-c-k-c0x00ffffff-no-rj",
+      country: "USA",
+      category: "Business"
+    }
+  ]
+};
